4933cf866a87198d21d70cecbaabd16536ff11ad
andrey-sever <andsul@mail.ru>
2026-04-14 13:52:13 +0300

    start

---
+++ graph_function.py
@@ -0,0 +1,5 @@
+from sympy import *
+
+x = symbols('x')
+f = x**2 - 10*x + 6
+plot(f)
---
+++ start.py
@@ -0,0 +1,11 @@
+def f(x):
+    return x**2
+
+x_val = [2, 62, 88, 100, 222]
+
+for x in x_val:
+    y = f(x)
+    print(y)
+
+x2 = int(input("Введите число: \n"))
+print(f(x2))
